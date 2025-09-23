8849e978687e3766c1ec71ec9e888ab9c403fdfd
AbhayPatil-005 <abhay.official005@gmail.com>
2025-09-23 22:33:52 +0530

    Introduction to redux in react

---
+++ Redux_In_React/redux_demo.js
@@ -0,0 +1,30 @@
+const redux = require('redux');
+
+//reducer function
+const counterReducer = (state={counter : 1}, action) => {
+        if(action.type==="INCREMENT"){
+            return{counter: state.counter + 1}
+        }else if(action.type==="DECREMENT"){
+            return{counter:state.counter - 1}
+        }else{
+            return state;
+        }
+};
+
+//store
+const store = redux.createStore(counterReducer);
+
+console.log("initial state: ", store.getState())
+
+//subcribe
+const counterSubscriber = () =>{
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+//dispatch
+store.dispatch({type:"INCREMENT"})
+store.dispatch({type:"INCREMENT"})
+store.dispatch({type:"DECREMENT"})
